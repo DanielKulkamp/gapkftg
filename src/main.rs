65f8c0a96dd1b62f2d77f6e2df584b8cd828d4b0
DanielKulkamp <daniel.kulkamp@gmail.com>
2021-06-09 20:29:53 -0300

    a

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,56 @@
-use std::io;
 use rand::{Rng, thread_rng};
 
-fn main() {
-    println!("Generating Random Number");
-    let mut rng = thread_rng();
-    let mut a : f64 = rng.gen_range(0.0..1.0);
+struct Gene {
+    strategy: Vec<f64>,
+    score: u32,
+}
 
-    println!("{}", a);
+
+
+fn build_random_gene() -> Gene {
+    let mut rng = thread_rng();
+    let mut a: Vec<f64> = vec![0f64; 6 ];
+    a = a.into_iter().map(|item| { rng.gen_range(0f64..1f64)}).collect();
+    //let total : f64 = a.iter().fold(0f64, |acc,x| {acc+x});
+    //for item in a.iter_mut(){
+    //    *item = *item/total;
+    //} 
+    let mut gene = Gene {
+        strategy: a,
+        score: 0u32  
+    };
+    normalize(&mut gene);
+    gene
+}
+
+fn normalize(gene : &mut Gene) -> () {
+    let total : f64 = gene.strategy.iter().fold(0f64, |acc,x| {acc+x});
+    for item in gene.strategy.iter_mut(){
+        *item = *item/total;
+    }
+}
+
+fn main() {
+    const GOAL_PROBS : [f64; 6] = [ 0.75, 0.95, 0.85, 0.85, 1.0, 0.95];
+
+    
+    println!("Generating Random Kickers Popualtion");
+    let mut kickers : Vec<Gene> = Vec::new();
+    for i in 0..50 {
+        kickers.push(build_random_gene());
+        //println!("{:?}", kickers[i].strategy)
+    }
+
+    println!("Generating Random Keepers Popualtion");
+    let mut keepers : Vec<Gene> = Vec::new();
+    for i in 0..50 {
+        keepers.push(build_random_gene());
+        //println!("{:?}", kickers[i].strategy)
+    }
+
+    println!("Shuffling populations")
+    keepers.sort_by_key(|item| -> {item.score} )
+
 
 
 }
